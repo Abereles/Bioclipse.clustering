d0c29f2036224108d51de53f050c758244dac64a
Jeff Miller <jrmiller1959@yahoo.com>
2010-07-30 23:49:00 -0700

    Code to support ui, file selection and clustering of molecules found therein

---
+++ src/net/bioclipse/chem/clustering/ClusteringPerspectiveFactory.java
@@ -0,0 +1,14 @@
+package net.bioclipse.chem.clustering;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class ClusteringPerspectiveFactory implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
